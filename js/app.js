15979b00a7624a1eea231532b8641ada7e6d8e34
Talaa Zina <talazina@gmail.com>
2020-05-19 12:11:31 +0200

    Hide contact window

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var popWin = document.querySelector("#myModal");
+const openModal = function (){
+	
+	popWin.style.top = 0;
+}
+var contact = document.querySelector("#contact");
+contact.addEventListener("click",openModal)
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  popWin.style.top = "-1000px";
+}
